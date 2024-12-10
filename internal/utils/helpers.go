624d753ab3489ae49f1a441f69727b63e75fe001
Ricky004 <tanaydam000@gmail.com>
2024-12-10 23:38:06 +0530

    initialize project with directory structure

--- internal/utils/helpers.go
+++ internal/utils/helpers.go
@@ -1,9 +1,9 @@
-package main
+package utils
 
 import "log"
 
 // assert checks a condition and panics with a message if the condition is false
-func assert(condition bool, message ...string) {
+func Assert(condition bool, message ...string) {
 	if !condition {
 		if len(message) > 0 {
 			log.Panicf("Assertion failed: %s", message[0])
